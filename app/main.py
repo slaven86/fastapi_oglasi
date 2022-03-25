e5a43c652913a00136024124370ae3539c7a579f
slaven86 <slaven.lakota@gmail.com>
2022-03-25 12:20:07 +0100

    add admin role

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from . import auth, users, models, cars
+from . import auth, users, models, cars, admin, admin_cars
 from .database import engine, SessionLocal
 
 models.Base.metadata.create_all(bind=engine)
@@ -11,3 +11,5 @@ db = SessionLocal()
 app.include_router(auth.router)
 app.include_router(users.router)
 app.include_router(cars.router)
+app.include_router(admin.router)
+app.include_router(admin_cars.router)
